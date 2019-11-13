459d0e1fc2f0e2d37f786ca0a20b1d4c77237b34
Jared S <jaredjames2020@gmail.com>
2019-11-13 02:30:00 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -10,43 +10,12 @@ INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) V
 (9, 'I want to teach English in China', 'charity', 3000.00, '2013-06-30', '2013-09-30'),
 (10, 'Help save birds of paradise', 'charity', 5000.00, '2012-03-20', '2013-06-30');
 
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(1, 'Help me buy a guitar', 'music', 500.00, '2013-06-30', '2013-07-30');
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(2, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(3, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(4, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(5, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(6, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(7, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(8, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(9, "abc", null, null, null, null);
--- INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES(10, "abc", null, null, null, null);
 
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17), (2, 'Bear', 6), (3, 'Iguana', 4), (4, 'Alex', 33),
 (5, 'Amanda', 24), (6, 'Sophie', 24), (7, 'Rosey', 9), (8, 'Victoria', 23), (9, 'Franz', 100), (10, 'Hermione', 30),
 (11, 'Voldemort', 90), (12, 'Marisa', 24), (13, 'Swizzle', 4), (14, 'Sirius', 36), (15, 'Albus', 113), (16, 'Squid', 5),
 (17, 'Whale', 6), (18, 'Pacha', 5), (19, 'Ena', 24), (20, 'Katie', 24);
  
- 
--- INSERT INTO users VALUES(1, "Ron", null);
--- INSERT INTO users VALUES(2, "Ron", null);
--- INSERT INTO users VALUES(3, "Ron", null);
--- INSERT INTO users VALUES(4, "Ron", null);
--- INSERT INTO users VALUES(5, "Ron", null);
--- INSERT INTO users VALUES(6, "Ron", null);
--- INSERT INTO users VALUES(7, "Ron", null);
--- INSERT INTO users VALUES(8, "Ron", null);
--- INSERT INTO users VALUES(9, "Ron", null);
--- INSERT INTO users VALUES(10, "Ron", null);
--- INSERT INTO users VALUES(11, "Ron", null);
--- INSERT INTO users VALUES(12, "Ron", null);
--- INSERT INTO users VALUES(13, "Ron", null);
--- INSERT INTO users VALUES(14, "Ron", null);
--- INSERT INTO users VALUES(15, "Ron", null);
--- INSERT INTO users VALUES(16, "Ron", null);
--- INSERT INTO users VALUES(17, "Ron", null);
--- INSERT INTO users VALUES(18, "Ron", null);
--- INSERT INTO users VALUES(19, "Ron", null);
--- INSERT INTO users VALUES(20, "Ron", null);
 
 INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 (1, 10.00, 1, 2),
@@ -79,34 +48,3 @@ INSERT INTO pledges (id, amount, user_id, project_id) VALUES
 (28, 90.00, 17, 9),
 (29, 230.00, 16, 6),
 (30, 450.00, 15, 5);
-
--- INSERT INTO pledges VALUES (1, 30, null, null);
--- INSERT INTO pledges VALUES (2, 30, null, null);
--- INSERT INTO pledges VALUES (3, 30, null, null);
--- INSERT INTO pledges VALUES (4, 30, null, null);
--- INSERT INTO pledges VALUES (5, 30, null, null);
--- INSERT INTO pledges VALUES (6, 30, null, null);
--- INSERT INTO pledges VALUES (7, 30, null, null);
--- INSERT INTO pledges VALUES (8, 30, null, null);
--- INSERT INTO pledges VALUES (9, 30, null, null);
--- INSERT INTO pledges VALUES (10, 30, null, null);
--- INSERT INTO pledges VALUES (11, 30, null, null);
--- INSERT INTO pledges VALUES (12, 30, null, null);
--- INSERT INTO pledges VALUES (13, 30, null, null);
--- INSERT INTO pledges VALUES (14, 30, null, null);
--- INSERT INTO pledges VALUES (15, 30, null, null);
--- INSERT INTO pledges VALUES (16, 30, null, null);
--- INSERT INTO pledges VALUES (17, 30, null, null);
--- INSERT INTO pledges VALUES (18, 30, null, null);
--- INSERT INTO pledges VALUES (19, 30, null, null);
--- INSERT INTO pledges VALUES (20, 30, null, null);
--- INSERT INTO pledges VALUES (21, 30, null, null);
--- INSERT INTO pledges VALUES (22, 30, null, null);
--- INSERT INTO pledges VALUES (23, 30, null, null);
--- INSERT INTO pledges VALUES (24, 30, null, null);
--- INSERT INTO pledges VALUES (25, 30, null, null);
--- INSERT INTO pledges VALUES (26, 30, null, null);
--- INSERT INTO pledges VALUES (27, 30, null, null);
--- INSERT INTO pledges VALUES (28, 30, null, null);
--- INSERT INTO pledges VALUES (29, 30, null, null);
--- INSERT INTO pledges VALUES (30, 30, null, null);
